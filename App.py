d26ddd7bf611a73589e7647d1693d95e87b6685a
root <nico.scognamiglio@gmail.com>
2020-06-08 22:47:22 +0000

    grr

--- App.py
+++ App.py
@@ -35,24 +35,22 @@ def parla_txt(testo):
     subprocess.run(["omxplayer","tts_out.mp3"])
 
 def check_identity(ip):
-    response = {}
-#    response['ip'] = ip
-#    out = run_cmd('sudo nmap -F '+ip,false).replace('"','')
-#    isDown = out.rfind("(0 hosts up)")
+    res = []
     
-#    if(isDown > 0):
-#        response['status'] = "OFFLINE"
-#    else:
-#        response['status'] = "ONLINE"
+    out = run_cmd('sudo nmap -F '+ip,false).replace('"','')
+    isDown = out.rfind("(0 hosts up)")
+    if(isDown > 0):
+        res['status'] = "OFFLINE"
+    else:
+        res['status'] = "ONLINE"
 #        wr = 'MAC Address: '
 #        wrl = len(wr)
 #        wf = len("XX:XX:XX:XX:XX:XX")
 #        wi = out.rfind(wr)
 #        response['mac'] = out[wi+wrl:wi+wrl+wf]
-        
 #    if(allFlg):
-    response['ip'] = ip
-    return response
+
+    return json.dumps(res)
 
 def list_hosts_up():
     out = run_cmd('nmap -v -sn 192.168.1.*',false)
