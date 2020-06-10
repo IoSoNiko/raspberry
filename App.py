49ba56a5ab42882606deba525ed31e98371f4251
IoSoNiko <45022699+IoSoNiko@users.noreply.github.com>
2020-06-10 02:38:56 +0200

    Update App.py

--- App.py
+++ App.py
@@ -1,29 +1,24 @@
-import os
+import os 
 import subprocess
 from flask import Flask , render_template , request
 from gtts import gTTS
 import json
-
+import sqlite3
 
 app = Flask(__name__)
 true = 1
 false = 0
+DB_NAME = "home_control"
+
 
 def isTrue(stato):
     return stato > 0
 
 def run_cmd(cmd,input):
-    print("START run_cmd >> cmd:'"+cmd+"' input:'"+str(input) if input else "" +"'")
     if(isTrue(input)):
         return os.system(cmd)
     else:
-        out = str(subprocess.check_output([cmd],shell=True,stderr=subprocess.PIPE))
-        print("END run_cmd >> out:'"+out+"'")
-        return out
-
-def git_pull():
-    print("START git_pull >> ")
-    return run_cmd('sudo git pull',false)
+        return str(subprocess.check_output([cmd],shell=True,stderr=subprocess.PIPE))
     
 def format_amixer_output(out):
     perc = out.rfind('%')
@@ -42,32 +37,58 @@ def parla_txt(testo):
     tts.save('tts_out.mp3')
     subprocess.run(["omxplayer","tts_out.mp3"])
 
-def find_my_ip():
-    print("START find_my_ip >>")
-    out = run_cmd('ifconfig',false)
-    ipP = out.find('192.168.1.')
-    print("INFO find_my_ip >> ipP: "+str(ipP))
-    my_ip = out[ipP:ipP+len("192.168.1.255")]
-    print("END find_my_ip >> my_ip: "+my_ip)
+def create_connection(db_file):
+    print("START create_connection >> db_file:"+db_file)
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+    print("END create_connection >>")
+    return conn
     
-def list_hosts_up(allFlg):
-    print("START list_hosts_up >> allFlg:'"+str(allFlg)+"'")
-    my_ip = find_my_ip()
+def get_all_users():
+    print("START get_all_users >>")
+    
+    cur = create_connection(DB_NAME).cursor()
+    cur.execute("SELECT * FROM USER")
+    rows = cur.fetchall()
+    print("END get_all_users >> rows:"+rows)
+    return rows
+    
+def search_identity(mac):
+    all_users = get_all_users()
+    for row in rows:
+        if(row['mac'] == mac)
+            return row
+
+def check_identity(ip):
+    out = run_cmd('sudo nmap -O '+ip,false)
+    split2 = out.split('MAC Address')
+    
+    parentesi = split.rfind('(')
+    mac = split[parentesi:parentesi+15]
+    response = {}
+    response['ip'] = ip
+    response['mac'] = mac
+    response['full'] = out
+    response['row'] = search_identity(mac)
+    return response
+
+
+def list_hosts_up():
     out = run_cmd('nmap -v -sn 192.168.1.*',false)
     splitted = out.split('Host is up')
     final = []
     if(len(splitted) > 1):
-            print("INFO list_hosts_up >> Ci sono circa "+str(len(splitted))+" hosts up")
             for val in splitted:
-                perc = val.rfind('192.168.1.')
-                obj = val[perc: perc + 13].replace('n','').replace('\\','')
+                str = val
+                perc = str.rfind('192.168.1.')
+                obj = str[perc: perc + 13].replace('n','').replace('\\','')
                 if(len(obj) > 0):
-                    print("INFO list_hosts_up >> "+obj+" is up")
-                    final.append(check_identity(obj,my_ip,None) if allFlg else obj)
-    
-    res = json.dumps(final)
-    print("END list_hosts_up >> res:'"+res+"'")
-    return res
+                    final.append(check_identity(obj))
+
+    return json.dumps(final)
 
 @app.route('/parla/<testo>')
 def parla(testo):
@@ -76,58 +97,11 @@ def parla(testo):
 
 @app.route('/sorveglia')
 def sorveglia():
-    
-    return list_hosts_up(request.args.get('all'))
-
-def extract_mac(out):
-    print("START extract_mac >>"+out)
-    wr = 'MAC Address: '
-    wrl = len(wr)
-    wf = len("XX:XX:XX:XX:XX:XX")
-    wi = out.rfind(wr)
-    mac = out[wi+wrl:wi+wrl+wf]
-    print("INFO extract_mac >> mac: "+mac)
-    if(mac[2:3] == ':'):
-        return mac
-    else:
-        return ""
-    
-    
-def check_identity(ip,my_ip,allFlg):
-    res = {}
-    res['ip'] = ip
-    res['status'] = "ONLINE"
-    if(my_ip == ip):
-        return res
-    
-    out = run_cmd('sudo nmap -F '+ip,false).replace('"','')
-    isDown = out.rfind("(0 hosts up)")
-    if(isDown > 0):
-        res['status'] = "OFFLINE"
-    else:
-        res['mac'] = extract_mac(out)
-    if(allFlg):
-        res['all'] = out
-        
-    return res
-
-@app.route('/sorveglia/<ip>')
-def sorverglia_ip(ip):
-    my_ip = ""
-    return json.dumps(check_identity(ip,my_ip,request.args.get('all')))
+    return list_hosts_up()
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/pull')
-def pull():
-    return git_pull()
-
-@app.route('/giambo')
-def giambo():
-    return 'Giambo'
-
 
 @app.route('/SETTINGS/')
 def options():
